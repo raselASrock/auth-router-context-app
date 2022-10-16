5b5e609cc2d3ff513902a05a8d3734cb2d51671b
raSel <mdraselm413@gmail.com>
2022-10-16 18:38:56 +0600

    context

---
+++ src/Context/UserContext.js
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react';
+
+const AuthContext = createContext();
+
+const UserContext = ({children}) => {
+    const user = {displayName:'Akash'}
+
+    const authInfo = {user: user}
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
